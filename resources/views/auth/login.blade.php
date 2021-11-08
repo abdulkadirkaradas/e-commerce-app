0d6ba4e611f290f73e80c21b699e6b350f0773b6
Abdulkadir Karadaş <kadirkaradas5@gmail.com>
2021-11-08 13:58:15 +0300

    main page was created. navbar structure basically was  created. layouts, partials and main view basically was created.

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.customer')
+@section('content')
+
+<h1>Hello</h1>
+
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,3 @@
+<nav style="width: 100%; position: absolute; top: 0; left: 0;">
+    <div>Navbar</div>
+</nav>
